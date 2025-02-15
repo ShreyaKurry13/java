a92ff8b1967d55f0b385134d7c60296fcb919a65
ShreyaKurry13 <shreyakurry13@gmail.com>
2025-02-15 14:12:34 +0530

    updated my practice

---
+++ CodeWithShreya/CWS_03_variables.java
@@ -0,0 +1,13 @@
+public class CWS_03_variables {
+
+    public static void main(String[] args) {
+        // video 3 
+
+        System.out.print("The sum of these numbers is: ");
+        int num1 = 4;
+        int num2 = 5;
+        int num3 = 6;
+        int sum= num1+num2+num3;
+        System.out.println(sum);
+}
+}
---
+++ CodeWithShreya/CWS_04_literals.java
@@ -0,0 +1,25 @@
+public class CWS_04_literals {
+    public static void main(String[] args) {
+        // video 4 Literals
+
+        byte a = 35;
+        int age = 56;
+        short s = 10000;
+        long l = 10000000000l;
+        boolean b = true;
+        float f = 45.67f;
+        double d = 2000.5420d;
+        char c = 'A';
+        System.out.println(a);
+        System.out.println(age);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(b);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+                        
+
+    }
+    
+}
