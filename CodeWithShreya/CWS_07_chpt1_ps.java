8e56498c70b57219f4a5385fc2030e092acd2e45
ShreyaKurry13 <shreyakurry13@gmail.com>
2025-02-17 20:46:09 +0530

    chapter 1 done

--- CodeWithShreya/CWS_07_chpt1_ps.java
+++ CodeWithShreya/CWS_07_chpt1_ps.java
@@ -1,7 +1,40 @@
+import java.util.Scanner;
+
 public class CWS_07_chpt1_ps {
 
     public static void main(String[] args) {
         //chapter 1 practice set
         //Question 1
+
+        // int a= 10,b=20, c=30;
+        // int sum = a+b+c;
+        // System.out.println("Sum of a, b, c is: "+sum);
+
+        //Question 2
+        // float phy = 60, chem = 75, maths = 80;
+        // float cgpa = (phy + chem + maths)/30;
+        // System.out.println("CGPA is: "+cgpa);
+
+        //Question 3
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter your name: ");
+        // String str = sc.nextLine();
+
+        // System.out.println("Hello "+str + ", Have a good day!");
+
+
+        //Question 4
+        // float miles = 0.6213712f;
+        // float km = 6;
+
+        // float conversion = km * miles;
+        // System.out.println("Conversion: "+conversion);
+
+        //Question 6
+        System.out.println("Enter your number: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+
     }
 }
