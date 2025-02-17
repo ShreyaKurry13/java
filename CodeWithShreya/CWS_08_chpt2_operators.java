3a5e0dcb96a29f77bd5e52a83fd971630fb5c3fe
ShreyaKurry13 <shreyakurry13@gmail.com>
2025-02-17 22:37:43 +0530

    9 videos donee

---
+++ CodeWithShreya/CWS_08_chpt2_operators.java
@@ -0,0 +1,28 @@
+public class CWS_08_chpt2_operators {
+    public static void main(String[] args) {
+        // 1. Arithmetic operators
+        // int a = 4;
+        // int b = 6 % a; // modulo operator --> returns the remainder when 4 is divided by 6
+        // System.out.println(b); 
+
+        // 2. Assignment operators
+    //    int a=6;
+    //    a+=4; // a = a + 4   4 gets added to 6
+    //    System.out.println(a); // prints 10
+
+       // 3. Comparison operators
+    //    System.out.println(8>2);  // returns true or false 
+
+       // 4. Logical operators
+    //    System.out.println(8>5 && 8>3); 
+    //    System.out.println(8<5 || 8<3);
+
+       // 5. Bitwise operators
+       int a = 5; // 101 in binary
+       int b = 3; // 011 in binary
+       int c = a & b; // 001 in binary
+       System.out.println(c); // prints 1
+
+    }
+}
+
---
+++ CodeWithShreya/CWS_09_pre.java
@@ -0,0 +1,30 @@
+public class CWS_09_pre {
+
+    public static void main(String[] args) {
+        // precedence and associativity of operators
+        /* 
+        int a = 5 + 3 * 2;     // importance of * is more than +
+        System.out.println(a); 
+
+        int b = 6*5-34/2; // importance of * is more than / and - is more than +
+        System.out.println(b); // 6*5 = 30, 34/2 = 17, 30- 17 = 13
+
+        */
+
+//priority series --->    * / + - 
+        // quick quiz
+        // int x=3;
+        // int y=4;
+        // int k = x+y/2;
+        // System.out.println(k); // 4/2 = 2, 3+2 = 5 
+
+
+        int a=2 , b=2,c= 1;
+        int d = b * b - 4* a *c / 2* a; //Multiplication (*) and division (/) have the same precedence.
+                                        // Operators of the same precedence are evaluated left to right.
+                                        //Java incorrectly interprets the denominator due to missing parentheses. ---->()
+
+              // 2*2-4*2*1/2*2   --> 4-4*2*1/2*2 -->  4-8*1/2*2  --> 4-8/2*2  -->  4-4*2  ---> 4-8 ---> -4                       
+        System.out.println(d); 
+    }
+}
