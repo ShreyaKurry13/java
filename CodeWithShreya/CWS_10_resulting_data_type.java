c789000bd7760c992357d482903eaf42b9f6f268
ShreyaKurry13 <shreyakurry13@gmail.com>
2025-02-20 20:44:40 +0530

    2 chapters done finally

--- CodeWithShreya/CWS_10_resulting_data_type.java
+++ CodeWithShreya/CWS_10_resulting_data_type.java
@@ -1,6 +1,26 @@
 public class CWS_10_resulting_data_type {
    
     public static void main(String[] args) {
-        
+        // byte x = 5;
+        // short y = 10;
+        // int z = 7;
+
+        // long l = 1000000000L;
+        // float f = 5.5f;
+        // float r = l+f;
+
+        // System.out.println(r);
+
+        //Increment and decrement of operators
+        int x = 5;
+        // System.out.println(x++);
+        // System.out.println(x); 
+        // System.out.println(++x);
+ 
+        int a= 11;
+        System.out.println(++a*3);
+        System.out.println(a);
+
+
     }
 }
---
+++ CodeWithShreya/CWS_12_chpt2_ps.java
@@ -0,0 +1,32 @@
+public class CWS_12_chpt2_ps {
+    public static void main(String[] args) {
+
+        //Question 1
+        // float a = 7/4.0f * 9/2.0f;
+        // System.out.println(a);
+
+        // Question 2
+        char grade = 'A';
+        grade = (char)(grade +8);
+        // System.out.println(grade);
+
+        //decrement
+        grade = (char)(grade -8);
+        // System.out.println(grade);
+
+        // Question 3
+        int x = 5;
+        // System.out.println(x>10);
+
+        // Question 4
+        float v = 5, u = 4, a= 3,s = 1;
+        float result = (v*v - u*u)/(2*a*s); 
+        System.out.println(result);
+
+
+        // Question 5   
+        int r = 7;
+        int p = 7*49/7+35/7;
+        System.out.println(p);
+    }
+}
